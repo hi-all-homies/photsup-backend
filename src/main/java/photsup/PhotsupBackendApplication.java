4ed2eaf7f127101dd615e63875276f4355b42149
hi-all-homies <kissfm.tratata@gmail.com>
2022-08-06 17:07:03 +0300

    first commit

---
+++ src/main/java/photsup/PhotsupBackendApplication.java
@@ -0,0 +1,13 @@
+package photsup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotsupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotsupBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/photsup/PhotsupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package photsup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotsupBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
